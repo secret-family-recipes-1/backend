119597041da71c10e1547f7d47ace0c68e675fd1
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-24 13:56:54 -0500

    delted test script

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors')
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
+// const authenticate = require('../auth/authenticate-middleware.js');
+// const authRouter = require('../auth/auth-router.js');
 
 // const recipesRouter = require('../routers/recipes/recipes-router')
 // const categoriesRouter = require('../routers/categories/router')
