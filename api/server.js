9832d61457fb4663d285008e7a56c6382186656b
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-24 14:05:35 -0500

    delted test script

--- api/server.js
+++ api/server.js
@@ -6,9 +6,9 @@ const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 
 const recipesRouter = require('../routers/recipes/recipes-router')
-// const categoriesRouter = require('../routers/categories/router')
-// const ingredientsRouter = require('../routers/ingredients/ingredients-router')
-// const stepsRouter = require('../routers/steps/steps-router')
+const categoriesRouter = require('../routers/categories/router')
+const ingredientsRouter = require('../routers/ingredients/ingredients-router')
+const stepsRouter = require('../routers/steps/steps-router')
 
 const server = express();
 
@@ -19,9 +19,9 @@ server.use(express.json());
 
 server.use('/api/auth', authenticate, authRouter);
 server.use('/api/recipes', authenticate,  recipesRouter)
-// server.use('/api/categories', authenticate, categoriesRouter)
-// server.use('/api/ingredients', authenticate, ingredientsRouter)
-// server.use('/api/steps', authenticate, stepsRouter )
+server.use('/api/categories', authenticate, categoriesRouter)
+server.use('/api/ingredients', authenticate, ingredientsRouter)
+server.use('/api/steps', authenticate, stepsRouter )
 
 
 
