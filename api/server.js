3ede4af8987cbefe5107eb7c56084d9ecfb84b27
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-24 13:49:35 -0500

    delted test script

--- api/server.js
+++ api/server.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 
-const recipesRouter = require('../routers/recipes/recipes-router')
-const categoriesRouter = require('../routers/categories/router')
-const ingredientsRouter = require('../routers/ingredients/ingredients-router')
-const stepsRouter = require('../routers/steps/steps-router')
+// const recipesRouter = require('../routers/recipes/recipes-router')
+// const categoriesRouter = require('../routers/categories/router')
+// const ingredientsRouter = require('../routers/ingredients/ingredients-router')
+// const stepsRouter = require('../routers/steps/steps-router')
 
 const server = express();
 
@@ -17,11 +17,11 @@ server.use(cors())
 server.use(express.json());
 
 
-// server.use('/api/auth', authRouter);
-// server.use('/api/recipes',  recipesRouter)
-// server.use('/api/categories', categoriesRouter)
-// server.use('/api/ingredients', ingredientsRouter)
-// server.use('/api/steps', stepsRouter )
+// server.use('/api/auth', authenticate, authRouter);
+// server.use('/api/recipes', authenticate,  recipesRouter)
+// server.use('/api/categories', authenticate, categoriesRouter)
+// server.use('/api/ingredients', authenticate, ingredientsRouter)
+// server.use('/api/steps', authenticate, stepsRouter )
 
 
 
