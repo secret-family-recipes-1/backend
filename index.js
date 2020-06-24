8d225efd4837150f573882e3e5a00a625e896343
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-24 12:51:04 -0500

    delted test script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js');
 require("dotenv").config()
 
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
