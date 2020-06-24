9619f41d337e59497c0c8e44bce792383f9a7615
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-24 12:25:42 -0500

    index to server

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-pgconnection = process.env.DATABASE_URL
+pgconnection = process.env.DATABASE_URL || "postgres://uytitphharkqzg:982735eda48f9d00929e2a087ca562ffaa3511cf094d02dadd274db7af9726f0@ec2-3-222-150-253.compute-1.amazonaws.com:5432/d47pbcru9ugdm2"
 module.exports = {
 
   development: {
